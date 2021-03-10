1bf44bfcb8bd32001555fb56cecf55a05ef5b4f1
Daniel Jordanov <62522030+danielgo26@users.noreply.github.com>
2021-03-10 20:01:38 +0200

    Add files via upload

---
+++ MoviesApp/MoviesApp/Data/Model/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+namespace MoviesApp.Data.Model
+{
+    [Table("Movies")]
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string MovieTitle { get; set; }
+        public int MovieYear { get; set; }
+        public int Duration { get; set; }
+        public string MovieCountry { get; set; }
+        public string Description { get; set; }
+        public IList<MovieActor> MoviesActors { get; set; }
+        public Movie(string movieTitle, int movieYear, int duration, string movieCountry, string description)
+        {
+            MovieTitle = movieTitle;
+            MovieYear = movieYear;
+            Duration = duration;
+            MovieCountry = movieCountry;
+            Description = description;
+        }
+    }
+}
