f0a4f6e72964a25a2b278038dd2577e6a8ded2b5
Daniel Jordanov <62522030+danielgo26@users.noreply.github.com>
2021-03-10 19:58:03 +0200

    Add files via upload

---
+++ ProjectFinalVersion/MoviesApp/MoviesApp/Business/MovieBusiness.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using MoviesApp.Data;
+using MoviesApp.Data.Model;
+using System.Linq;
+
+namespace MoviesApp.Business
+{
+    class MovieBusiness
+    {
+       
+    }
+}
